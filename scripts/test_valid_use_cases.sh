eec745afe078fffee134d6990ecf0764cb851160
Victor Pereira <vpereirabr@gmail.com>
2023-11-26 21:14:01 +0100

    Add FTP docker service and integration test

--- scripts/test_valid_use_cases.sh
+++ scripts/test_valid_use_cases.sh
@@ -7,5 +7,6 @@ $BRUCUTU -u pop3://email -l foo -p thepassword || exit -1
 $BRUCUTU -u pop3://email -L samples/users.txt -P samples/passwd.txt || exit -1
 # Giving false positive
 $BRUCUTU -u http://httpd -l foo -p bar -f || exit -1
+$BRUCUTU -u ftp://ftp -l foo -p bar -f || exit -1
 
 exit 0
