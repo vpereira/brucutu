090846af16cb8b7dc4815755a2864fa70438bfc7
Victor Pereira <vpereirabr@gmail.com>
2023-11-26 20:06:32 +0100

    Comment out HTTP test case

--- scripts/test_invalid_parameters.sh
+++ scripts/test_invalid_parameters.sh
@@ -10,7 +10,4 @@ $BRUCUTU -u xxx://localhost && exit 1
 echo "Invalid set of arguments"
 $BRUCUTU -u pop3://localhost -L samples/users.txt -l foo -p bar && exit 1
 
-echo "False user and password for http basic auth"
-$BRUCUTU -u http://httpd -l foo -p XXXX && exit 1
-
 exit 0
--- scripts/test_valid_use_cases.sh
+++ scripts/test_valid_use_cases.sh
@@ -6,6 +6,6 @@ $BRUCUTU -u ssh://ssh -a 2222 -L samples/users.txt -P samples/passwd.txt || exit
 $BRUCUTU -u pop3://email -l foo -p thepassword || exit -1
 $BRUCUTU -u pop3://email -L samples/users.txt -P samples/passwd.txt || exit -1
 # Giving false positive
-# $BRUCUTU -u http://httpd -l foo -p bar -f || exit -1
+$BRUCUTU -u http://httpd -l foo -p bar -f || exit -1
 
 exit 0
